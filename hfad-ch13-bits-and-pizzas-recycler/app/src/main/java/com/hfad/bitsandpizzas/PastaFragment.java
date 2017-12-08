757aaaae891e543fe28f08afbe79d7328070bf46
Danilo Mutti <dmutti@gmail.com>
2017-12-08 16:57:20 -0200

    mais codigo

--- hfad-ch13-bits-and-pizzas-recycler/app/src/main/java/com/hfad/bitsandpizzas/PastaFragment.java
+++ hfad-ch13-bits-and-pizzas-recycler/app/src/main/java/com/hfad/bitsandpizzas/PastaFragment.java
@@ -2,24 +2,35 @@ package com.hfad.bitsandpizzas;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.ListFragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 
-public class PastaFragment extends ListFragment {
-
-    private static final String[] pastas = { "Spaghetti Bolognese", "Lasagne" };
+public class PastaFragment extends Fragment {
 
     public PastaFragment() { }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(inflater.getContext(), android.R.layout.simple_list_item_1, pastas);
-        setListAdapter(adapter);
-        return super.onCreateView(inflater, container, savedInstanceState);
+        RecyclerView pastaRecycler = (RecyclerView) inflater.inflate(R.layout.fragment_pasta, container, false);
+
+        String[] pastaNames = new String[Pasta.pastas.length];
+        int[] pastaImages = new int[Pasta.pastas.length];
+
+        for (int i = 0; i < Pasta.pastas.length; i++) {
+            pastaNames[i] = Pasta.pastas[i].getName();
+            pastaImages[i] = Pasta.pastas[i].getImageResourceId();
+        }
+
+        CaptionedImagesAdapter adapter = new CaptionedImagesAdapter(pastaNames, pastaImages);
+        pastaRecycler.setAdapter(adapter);
+
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(getActivity(), 2);
+        pastaRecycler.setLayoutManager(gridLayoutManager);
+
+        return pastaRecycler;
     }
 
 }
--- hfad-ch13-bits-and-pizzas-recycler/app/src/main/java/com/hfad/bitsandpizzas/PizzaFragment.java
+++ hfad-ch13-bits-and-pizzas-recycler/app/src/main/java/com/hfad/bitsandpizzas/PizzaFragment.java
@@ -1,23 +1,36 @@
 package com.hfad.bitsandpizzas;
 
 import android.os.Bundle;
-import android.support.v4.app.ListFragment;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 
-public class PizzaFragment extends ListFragment {
-
-    private static final String[] pizzas = { "Diavolo", "Funghi" };
+public class PizzaFragment extends Fragment {
 
     public PizzaFragment() { }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(inflater.getContext(), android.R.layout.simple_list_item_1, pizzas);
-        setListAdapter(adapter);
-        return super.onCreateView(inflater, container, savedInstanceState);
+        RecyclerView pizzaRecycler = (RecyclerView) inflater.inflate(R.layout.fragment_pizza, container, false);
+
+        String[] pizzaNames = new String[Pizza.pizzas.length];
+        int[] pizzaImages = new int[Pizza.pizzas.length];
+
+        for (int i = 0; i < Pizza.pizzas.length; i++) {
+            pizzaNames[i] = Pizza.pizzas[i].getName();
+            pizzaImages[i] = Pizza.pizzas[i].getImageResourceId();
+        }
+
+        CaptionedImagesAdapter adapter = new CaptionedImagesAdapter(pizzaNames, pizzaImages);
+        pizzaRecycler.setAdapter(adapter);
+
+        GridLayoutManager layoutManager = new GridLayoutManager(getActivity(), 2);
+        pizzaRecycler.setLayoutManager(layoutManager);
+
+        return pizzaRecycler;
     }
 
 }
