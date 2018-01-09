cb9798c8354bfd8e7052ea17930d9b1140a89d3a
Danilo Mutti <dmutti@gmail.com>
2018-01-08 22:55:25 -0200

    ch 18

--- hfad-ch18-joke/app/src/main/java/com/hfad/joke/DelayedMessageService.java
+++ hfad-ch18-joke/app/src/main/java/com/hfad/joke/DelayedMessageService.java
@@ -1,12 +1,16 @@
 package com.hfad.joke;
 
 import android.app.IntentService;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 public class DelayedMessageService extends IntentService {
 
     public static final String EXTRA_MESSAGE = "message";
+    public static final int NOTIFICATION_ID = 5453;
 
     public DelayedMessageService() {
         super("DelayedMessageService");
@@ -24,7 +28,20 @@ public class DelayedMessageService extends IntentService {
     }
 
     private void showText(String arg) {
-        Log.v("DelayedMessageService", "The message is: [" + arg + "]");
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
+                .setSmallIcon(android.R.drawable.sym_def_app_icon)
+                .setContentTitle(getString(R.string.question))
+                .setContentText(arg)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setVibrate(new long[] { 0, 1000 })
+                .setAutoCancel(true);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        builder.setContentIntent(pendingIntent);
+
+        NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        manager.notify(NOTIFICATION_ID, builder.build());
     }
 
 }
