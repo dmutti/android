9a0188098f9639a40d3092fe62d19381a20de732
Danilo Mutti <dmutti@gmail.com>
2018-01-11 22:53:51 -0200

    ch 19

--- hfad-ch19-odometer/app/src/main/java/com/hfad/odometer/MainActivity.java
+++ hfad-ch19-odometer/app/src/main/java/com/hfad/odometer/MainActivity.java
@@ -1,13 +1,20 @@
 package com.hfad.odometer;
 
 import android.app.Activity;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.IBinder;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.content.ContextCompat;
 import android.widget.TextView;
 import java.util.Locale;
 
@@ -15,6 +22,8 @@ public class MainActivity extends Activity {
 
     private OdometerService odometer;
     private boolean bound = false;
+    private final int PERMISSION_REQUEST_CODE = 698;
+    private final int NOTIFICATION_ID = 423;
 
     private ServiceConnection connection = new ServiceConnection() {
         @Override
@@ -33,8 +42,13 @@ public class MainActivity extends Activity {
     @Override
     protected void onStart() {
         super.onStart();
-        Intent intent = new Intent(this, OdometerService.class);
-        bindService(intent, connection, Context.BIND_AUTO_CREATE);
+        if (ContextCompat.checkSelfPermission(this, OdometerService.PERMISSION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[] { OdometerService.PERMISSION }, PERMISSION_REQUEST_CODE);
+
+        } else {
+            Intent intent = new Intent(this, OdometerService.class);
+            bindService(intent, connection, Context.BIND_AUTO_CREATE);
+        }
     }
 
     @Override
@@ -66,5 +80,32 @@ public class MainActivity extends Activity {
                 handler.postDelayed(this, 1000);
             }
         });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_REQUEST_CODE: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Intent intent = new Intent(this, OdometerService.class);
+                    bindService(intent, connection, Context.BIND_AUTO_CREATE);
+                } else {
+                    NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
+                            .setSmallIcon(android.R.drawable.ic_menu_compass)
+                            .setContentTitle(getString(R.string.app_name))
+                            .setContentText(getString(R.string.permission_denied))
+                            .setPriority(NotificationCompat.PRIORITY_HIGH)
+                            .setVibrate(new long[] { 0, 1000 })
+                            .setAutoCancel(true);
+
+                    Intent intent = new Intent(this, MainActivity.class);
+                    PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+                    builder.setContentIntent(pendingIntent);
+
+                    NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+                    manager.notify(NOTIFICATION_ID, builder.build());
+                }
+            }
+        }
     }
 }
--- hfad-ch19-odometer/app/src/main/java/com/hfad/odometer/OdometerService.java
+++ hfad-ch19-odometer/app/src/main/java/com/hfad/odometer/OdometerService.java
@@ -1,19 +1,81 @@
 package com.hfad.odometer;
 
+import android.Manifest;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Binder;
+import android.os.Bundle;
 import android.os.IBinder;
+import android.support.v4.content.ContextCompat;
 import java.security.SecureRandom;
 
 public class OdometerService extends Service {
 
+    private LocationListener listener;
+    private LocationManager manager;
+    private static double distance;
+    private static Location lastLocation;
+
+    public static final String PERMISSION = Manifest.permission.ACCESS_FINE_LOCATION;
     private final OdometerBinder binder = new OdometerBinder();
-    private final SecureRandom random = new SecureRandom();
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        listener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                if (lastLocation == null) {
+                    lastLocation = location;
+                }
+                distance += location.distanceTo(lastLocation);
+                lastLocation = location;
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) { }
+
+            @Override
+            public void onProviderEnabled(String provider) { }
+
+            @Override
+            public void onProviderDisabled(String provider) { }
+        };
+
+        manager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (ContextCompat.checkSelfPermission(this, PERMISSION) == PackageManager.PERMISSION_GRANTED) {
+            String provider = manager.getBestProvider(new Criteria(), true);
+            if (provider != null) {
+                manager.requestLocationUpdates(provider, 1000, 1, listener);
+            }
+        }
+    }
 
     @Override
     public IBinder onBind(Intent intent) {
         return binder;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (manager == null || listener == null) {
+            return;
+        }
+
+        if (ContextCompat.checkSelfPermission(this, PERMISSION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+
+        manager.removeUpdates(listener);
+        manager = null;
+        listener = null;
     }
 
     public class OdometerBinder extends Binder {
@@ -23,6 +85,6 @@ public class OdometerService extends Service {
     }
 
     public double getDistance() {
-        return random.nextDouble();
+        return distance / 1609.344;
     }
 }
