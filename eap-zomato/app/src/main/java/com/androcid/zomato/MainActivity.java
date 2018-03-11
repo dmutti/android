b14f9613b9a72f1aca7a58dd96a088424cc25a04
Danilo Mutti <dmutti@gmail.com>
2018-03-10 22:11:42 -0300

    versions

--- eap-zomato/app/src/main/java/com/androcid/zomato/MainActivity.java
+++ eap-zomato/app/src/main/java/com/androcid/zomato/MainActivity.java
@@ -3,6 +3,7 @@ package com.androcid.zomato;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+//https://developers.google.com/mobile/add?platform=android&cntapi=signin&cnturl=https:%2F%2Fdevelopers.google.com%2Fidentity%2Fsign-in%2Fandroid%2Fsign-in%3Fconfigured%3Dtrue&cntlbl=Continue%20Adding%20Sign-In
 public class MainActivity extends AppCompatActivity {
 
     @Override
